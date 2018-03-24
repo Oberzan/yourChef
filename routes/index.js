b55e52c6b6f550f121eb5bc2a36f18c203b66446
Oberzan <oberzan.simon@gmail.com>
2018-03-24 13:04:08 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const index = require('../controllers/index.js');
+
+/* GET home page. */
+router.get('/', index.homepage);
+
+module.exports = router;
